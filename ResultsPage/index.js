b393145a7400a5e2498ea180971fa80ed97d0151
Matthew Bell <m.bell@cs.ucl.ac.uk>
2017-09-17 08:21:50 -0400

    Fix merge conflicts

--- ResultsPage/index.js
+++ ResultsPage/index.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
-import Styles from "../Styles";
+import Styles, { Colours } from "../Styles";
 
 class ResultsPage extends Component {
   render() {
     return (
-      <View style={[Styles.page, { alignItems: "center", justifyContent: "center" }]}>
-        <Text style={[Styles.headerText, { textAlign: "center" }]}>You can park here until</Text>
-        <TouchableOpacity style={Styles.button} onPress={() => this.props.history.push("/camera")}>
+      <View
+        style={[
+          Styles.page,
+          {
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: Colours.cyan,
+          },
+        ]}
+      >
+        <Text style={[Styles.headerText, { textAlign: "center" }]}>
+          You can park here until 12:00
+        </Text>
+        <TouchableOpacity
+          style={Styles.button}
+          onPress={() => this.props.history.push("/camera")}
+        >
           <Text style={Styles.buttonText}>Start Again</Text>
         </TouchableOpacity>
       </View>
--- Styles.js
+++ Styles.js
@@ -1,19 +1,28 @@
 import { StyleSheet } from "react-native";
 
+export const Colours = {
+  cyan: "#31B5BE",
+  blue: "#5B86E5",
+};
+
 export default Styles = StyleSheet.create({
   page: {
     flex: 1,
     flexDirection: "column",
     padding: 10,
-
   },
   button: {
     padding: 20,
     backgroundColor: "#31B5BE",
+    borderColor: "white",
+    borderWidth: 2,
     borderRadius: 30,
     shadowOffset:{  width: 0,  height: 4,  },
     shadowOpacity: 0.3,
     shadowRadius: 4,
+    backgroundColor: Colours.cyan,
+    margin: 10,
+    borderRadius: 20,
   },
   buttonText: {
     fontFamily: 'System',
@@ -26,5 +35,6 @@ export default Styles = StyleSheet.create({
     fontWeight: '500',
     fontSize: 30,
     marginBottom: 100,
+    color: "white",
   },
 });
